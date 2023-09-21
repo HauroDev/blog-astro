4769a4e773b16d17c3250c0143e22fc09e18cc1c
HauroEnojado <victormanu_el08@outlook.com>
2023-09-21 12:52:44 -0300

    Modificando: titulo del feed rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
   return rss({
-    title: 'Astro alumno | Blog',
+    title: 'HauroDev | Blog',
     description: 'Mi viaje de aprendizaje de Astro',
     site: 'https://blog-astro-haurodev.netlify.app/',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
